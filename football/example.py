6aa02894ac4a6ed9c9cade522f00dc6d02131f05
Hoeun Lee <hoeunlee228@gmail.com>
2021-05-25 22:34:11 +0900

    .

--- football/example.py
+++ football/example.py
@@ -1,16 +1,19 @@
 import gfootball.env as football_env
 
 env = football_env.create_environment(
-		env_name='11_vs_11_stochastic',
-		render=True,
-		number_of_left_players_agent_controls=11,
-		number_of_right_players_agent_controls=0)
+        env_name="11_vs_11_easy_stochastic",
+        rewards="scoring,checkpoints",
+        render=False,
+        number_of_left_players_agent_controls=11,
+        representation='simple115v2')
 env.reset()
 done = False
 
 while not done:
-	actions = []
-	for i in range(11):
-		actions.append(env.action_space.sample())
+    actions = env.action_space.sample()
 
-	observation, reward, done, info = env.step(actions)
+    observation, reward, done, info = env.step(actions)
+
+    print(observation.shape)
+    print(reward.shape)
+    print(done)
--- football/test2.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-
-a = [0.0 for k in range(19)]
-a[4] = 1.0
-
-print(np.argmax(a))
